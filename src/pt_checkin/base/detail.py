ef8e3ce9f8fa45247653bd3e57948580e59aaefc
wuyaos <wufeifeng_hust@163.com>
2025-08-14 10:21:52 +0800

    gx

--- src/pt_checkin/base/detail.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from ..core.entry import SignInEntry
-
-
-class Detail(ABC):
-    @abstractmethod
-    def get_details(self, entry: SignInEntry, config: dict) -> None:
-        pass
--- src/pt_checkin/base/message.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from ..core.entry import SignInEntry
-
-
-class Message(ABC):
-    @abstractmethod
-    def get_messages(self, entry: SignInEntry, config: dict) -> None:
-        pass
